10c3c2f10708108d8ac1cc2afcf958833f17538b
Guillem Ramírez Miranda <guillem.ramirez@ieee.org>
2019-10-21 10:35:11 +0200

    Added fmt, mux and net package for API handling. Added home endpoint. Added router and basic routing structure for the api.

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"log"
+	"net/http"
 )
+
+func GetHomeEndpoint(w http.ResponseWriter, req *http.Request) {
+	_, err := fmt.Fprintf(w, "Ez api")
+	if err != nil {
+		log.Fatal("Error in GetHomeEndpoint")
+	}
+}
 
 func main() {
 	log.Print("Init server.")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", GetHomeEndpoint).Methods("GET")
+	log.Fatal(http.ListenAndServe(":3001", router))
 }
