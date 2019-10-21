d678ca9d6a29a971eddf518209bb031a1e4c3c6b
Guillem Ramírez Miranda <guillem.ramirez@ieee.org>
2019-10-21 15:05:36 +0200

    Fixed parsing of name in json.

--- main.go
+++ main.go
@@ -16,20 +16,21 @@ func GetHomeEndpoint(w http.ResponseWriter, req *http.Request) {
 }
 
 type User struct {
-	name string
+	Name string `json:"name"`
 }
 
 func RegisterEndpoint(w http.ResponseWriter, req *http.Request) {
-	name := User{}
+	var user User
+
 	d := json.NewDecoder(req.Body)
-	if err := d.Decode(&name); err != nil {
+	if err := d.Decode(&user); err != nil {
 		http.Error(w, err.Error(), 400)
 		log.Print("Error register parsing")
 		log.Print(err.Error())
 		return
 	}
-	log.Printf("%s", name.name)
-	if _, err := fmt.Fprintf(w, "Welcome %s", name.name); err != nil {
+	log.Printf("%s", user.Name)
+	if _, err := fmt.Fprintf(w, "Welcome %s", user.Name); err != nil {
 		log.Fatal("Error register response")
 	}
 }
